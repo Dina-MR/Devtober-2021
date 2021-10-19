a033704697e1412dee4657ce4fac548580fde627
C0rvo <quan.uminh1@gmail.com>
2021-10-19 17:28:59 +0700

    Fix the dialogue and complete NPCs
    make dialogue use E instead of click

--- Vijngi game (Devtober 2021)/Assets/Scripts/SortingRender.cs
+++ Vijngi game (Devtober 2021)/Assets/Scripts/SortingRender.cs
@@ -8,14 +8,26 @@ public class SortingRender : MonoBehaviour
     [SerializeField] int sortingIndex = 5000;
     [SerializeField] int offset = 0;
     Renderer myRenderer;
+    float timer;
+    float timerMax = 0.25f;
+    [SerializeField] bool runOnce = false;
     void Start()
     {
+        timer = timerMax;
         myRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        myRenderer.sortingOrder = (int)(sortingIndex - transform.position.y + offset);
+        timer -= Time.deltaTime;
+        if(timer < 0){
+            timer = timerMax;
+            myRenderer.sortingOrder = (int)(sortingIndex - transform.position.y + offset);
+            if(runOnce)
+            {
+                Destroy(this);
+            }
+        }
     }
 }
