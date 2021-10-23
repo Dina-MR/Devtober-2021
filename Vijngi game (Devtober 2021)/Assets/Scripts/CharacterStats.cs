c5d390256977700958fb10d57e94702b881ca3db
DMR <dina.randriaza@gmail.com>
2021-10-24 00:51:06 +0200

    Updating character stats
    *The new class, StatModifier, could be used to update a statistic (either depending on buffs and debuffs or leveling up).

--- Vijngi game (Devtober 2021)/Assets/Scripts/CharacterStats.cs
+++ Vijngi game (Devtober 2021)/Assets/Scripts/CharacterStats.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    [SerializeField] public Stat level;
+    //[SerializeField] public Stat level;
 
-    [SerializeField] public Stat attack;
-    [SerializeField] public Stat defense;
-    [SerializeField] public Stat hp;
-    [SerializeField] public Stat tp;
-    [SerializeField] public Stat criticalChance;
-    [SerializeField] public Stat criticalAttack;
+    //[SerializeField] public Stat attack;
+    //[SerializeField] public Stat defense;
+    //[SerializeField] public Stat hp;
+    //[SerializeField] public Stat tp;
+    //[SerializeField] public Stat criticalChance;
+    //[SerializeField] public Stat criticalAttack;
+
+    public List<Stat> stats = new List<Stat>();
+
+    void Start(float attackValue, float defenseValue, float hpValue, float tpValue, float criticalChanceValue, float criticalAttackValue)
+    {
+        stats.Add(new Stat(attackValue, "Attack", "ATK"));
+        stats.Add(new Stat(defenseValue, "Defense", "DEF"));
+        stats.Add(new Stat(hpValue, "HP", "HP"));
+        stats.Add(new Stat(tpValue, "TP", "TP"));
+        stats.Add(new Stat(criticalChanceValue, "Critical Chance", "CRIT.CHA"));
+        stats.Add(new Stat(criticalAttackValue, "Critical Attack", "CRIT.ATK"));
+    }
 
     public void CharacterStatsFunction()
     {
@@ -21,19 +34,44 @@ public class CharacterStats : MonoBehaviour
     // Represents a stat
     public class Stat
     {
-        public float baseValue; //Represents the stat value at Level 1;
-        public float currentValue;
-        public float minimalValue;
-        public float maximalValue;
-        public float increaseValue; // Increases the value of the stat after leveling up
+        public float baseValue { get; set; } //Represents the stat value at Level 1;
+        public float currentValue { get; set; } //Represents the current value of the stat
+        public string statName { get; set; }
+        public string statAbbreviation { get; set; }
+        private List<StatModifier> statModifiers; //Lists the buffs and debuffs on the stat
 
-        public void Start(float _baseValue, float _currentValue, float _minimalValue, float _maximalValue, float _increaseValue)
+        public Stat(float _baseValue, string _statName, string _statAbbreviation)
         {
             this.baseValue = _baseValue;
-            this.currentValue = _currentValue;
-            this.minimalValue = _minimalValue;
-            this.maximalValue = _maximalValue;
-            this.increaseValue = _increaseValue;
+            this.statName = _statName;
+            this.statAbbreviation = statAbbreviation;
+        }
+
+        public void AddStatModifier(StatModifier statModifier)
+        {
+            this.statModifiers.Add(statModifier);
+        }
+
+        public void RemoveStatModifier(StatModifier statModifier)
+        {
+            this.statModifiers.Remove(statModifier);
+        }
+
+        public float GetCalculatedStatValue()
+        {
+            float newValue = this.baseValue + this.statModifiers.Sum(statModifier => statModifier.modifierValue); // Calculate the current value by adding to the base value the total of the modifiers values
+            return newValue;
+        }
+    }
+
+    // Represent buffs or debuffs on the stat
+    public class StatModifier
+    {
+        public float modifierValue { get; set; }
+
+        public StatModifier(float _modifierValue)
+        {
+            modifierValue = _modifierValue;
         }
     }
 }
